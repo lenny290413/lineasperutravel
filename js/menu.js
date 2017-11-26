d43e5766dd5a1d54a3d506f6113d0e4cb83f4435
lenin gonzales <lenny290413@gmail.com>
2017-11-25 19:39:00 -0500

    segunda seccion ya hecha

--- js/menu.js
+++ js/menu.js
@@ -8,3 +8,51 @@ document.getElementById("icon-menu").onclick =  function(){
 document.getElementById("menu").onclick =  function(){
     menu();
 }
+//almacenar slider en una variable
+var slider = $('#slider_ofertas');
+//almacenar botones
+var siguiente = $('#btn-next');
+var anterior = $('#btn-prev');
+
+//mover ultima imagen al primer lugar
+$('#promocion-items:last').insertBefore('#promocion-items:first');
+//mostrar la primera imagen con un margen de -100%
+slider.css('margin-left', '-'+100+'%');
+
+function moverD() {
+	slider.animate({
+		marginLeft:'-'+200+'%'
+	} ,700, function(){
+		$('.promociones-items:first').insertAfter('.promociones-items:last');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+function moverI() {
+	slider.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('.promociones-items:last').insertBefore('.promociones-items:first');
+		slider.css('margin-left', '-'+100+'%');
+	});
+}
+
+function autoplay() {
+	interval = setInterval(function(){
+		moverD();
+	}, 5000);
+}
+siguiente.on('click',function() {
+	moverD();
+	clearInterval(interval);
+	autoplay();
+});
+
+anterior.on('click',function() {
+	moverI();
+	clearInterval(interval);
+	autoplay();
+});
+
+
+autoplay();
